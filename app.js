443a9f0df7a9598c8c744ea81b7f06d24a15c4cb
Mohammad Jabir <mohammadj94@gmail.com>
2024-07-10 18:09:02 -0400

    exercise complete

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Mohammad"
+let lastName
+var age
 
 // Exercise 2
+firstName = "MohammadDos" // cannot change constant variable
+lastName = 'Jabir'
+age = 29
 
 // Exercise 3
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitive = true
+
+// Exercise 4
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSizes = "M"
+
+// Exercise 5
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 722
+let bookmark = 456
+let hasRead = true
